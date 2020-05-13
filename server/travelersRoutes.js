648c0057d396afed3ae1131ccd58ccdfeed5822e
Tina Xing <tinaxing14@gmail.com>
2020-05-13 15:14:51 -0400

    fix routes

--- server/travelersRoutes.js
+++ server/travelersRoutes.js
@@ -9,13 +9,19 @@ travelersRouter.get('/:id/price', (req, res) => {
   fetch(url)
     .then(response => response.json())
     .then(json => res.json(json))
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 travelersRouter.get('/:id/calendar/?', (req, res) => {
   const url= `${host}/api/trip${req.url}`
   fetch(url)
     .then(response => response.json())
-    .then(json => res.json(json));
+    .then(json => res.json(json))
+    .catch(err => {
+      console.log(err);
+    });
 })
 
 
